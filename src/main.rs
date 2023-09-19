cc0833dcab382594d87491fb2a4aceb01c99d29b
shoun97 <manuelurra2497@gmail.com>
2023-09-19 18:08:09 -0300

    .env added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL no está definida en el archivo .env");
+    let api_key = env::var("API_KEY").expect("API_KEY no está definida en el archivo .env");
+
+    println!("DATABASE_URL: {}", database_url);
+    println!("API_KEY: {}", api_key);
 }
